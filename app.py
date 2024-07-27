632f113ecda12af63664b470ed2b1742207c814b
Vicky Prayudha <97606022+VDpraja@users.noreply.github.com>
2024-07-27 07:52:11 +0000

    added database

--- app.py
+++ app.py
@@ -1,10 +1,43 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+
 app = Flask(__name__)
 
+JOBS = [
+  {
+    'id': 1,
+    'title': 'Bot Discord Simple',
+    'language': 'Python',
+    'price': '$150 *Include Hosting'
+  },
+  {
+    'id': 2,
+    'title': 'Bot Discord Utils',
+    'language': 'Python',
+    'price': '$250 *Include Hosting'
+  },
+  {
+    'id': 3,
+    'title': 'Bot Discord Moderation',
+    'language': 'Python',
+    'price': '$550 *Include Hosting'
+  },
+  {
+    'id': 4,
+    'title': 'Moderator Discord',
+    'language': 'English and Indonesian',
+    'price': '$350 for a month *REMOTE*'
+  }
+]
 
 @app.route("/")
 def hello_vicky():
-  return render_template('home.html')
+  return render_template('home.html',
+                        jobs=JOBS,
+                        company_name='Validity')
+
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
 
 
 if __name__ == "__main__":
